ed6b25165be47f900b47e8a489894651dc1d2ccd
orhan <orhan.pierre.france@gmail.com>
2023-10-10 16:53:43 +0200

    cc

--- sounds/api.py
+++ sounds/api.py
@@ -1,11 +1,13 @@
 from typing import Union
 import pathlib
 from abc import ABC,abstractmethod
-
+from datasets import Dataset
 
 class SoundGenerator(ABC):
     @abstractmethod
-    def generateSounds(self,dirWav: Union[str,pathlib.Path], dirZarr: Union[str,pathlib.Path]):
+    def generateSounds(self,
+                       dirWav: Union[str,pathlib.Path],
+                       dirDataset: Union[str,pathlib.Path]) -> Dataset:
         # Generates a set of experimental stimulis.
         #   Wav files are put in dirWav.
         #   Waveform are also put in the dirZarr directory but as a zarr groups.
---
+++ sounds/perExperiment/readme.md
@@ -0,0 +1,9 @@
+Sequences de tons
+Paroles
+
+
+What needs to be implemented for every experiments:
+
+One sound is composed of:
+    Random; Regular; Random or Deviant
+
