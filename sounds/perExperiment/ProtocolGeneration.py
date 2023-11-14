82aec5b0a085bf39f4b2f5e8df58d8ef2b74973d
julien-gadonneix <juliengado.2001@gmail.com>
2023-11-14 15:27:08 +0100

    :ControlledStim: Completing combinator class -> creating a dataset
    beginning of combinator
    
    ControlledStim : Completing combinator class -> creating a dataset

--- sounds/perExperiment/ProtocolGeneration.py
+++ sounds/perExperiment/ProtocolGeneration.py
@@ -8,13 +8,15 @@ from sounds.api import SoundGenerator
 import soundfile as sf
 import random
 
+RFRAM_key = None
 
 samplerates_sound = {
     "gaussian_N": 16000,
     "gaussian_RN": 16000,
-    "gaussian_RefN": 16000,
+    "gaussian_RefRN": 16000,
     "bip_1": 16000,
     "bip_2": 16000,
+    "silence": 16000
 }
 
 list_sounds_name = list(samplerates_sound)
@@ -22,22 +24,25 @@ list_sounds_name = list(samplerates_sound)
 durations_sound = {
     "gaussian_N": 1000,
     "gaussian_RN": 1000,
-    "gaussian_RefN": 1000,
+    "gaussian_RefRN": 1000,
     "bip_1": 1000,
     "bip_2": 1000,
+    "silence": 1000
 }
 consines_rmp_length = {
     "gaussian_N": 5,
     "gaussian_RN": 5,
-    "gaussian_RefN": 5,
+    "gaussian_RefRN": 5,
     "bip_1": 5,
     "bip_2": 5,
+    "silence": 5
 }
 
 from datasets import load_dataset, Audio
 
-def rfram_sequence() -> list[str]:
-    """Generates a correct RFRAM sequence.
+
+def rfram_sequence_exp1() -> list[str]:
+    """Generates a correct RFRAM_1 sequence.
 
     :return: list[str]
     """
@@ -56,14 +61,14 @@ def rfram_sequence() -> list[str]:
         if sequence[i] == refRN and sequence[i - 1] == refRN:
             while sequence[i] == refRN:
                 j = random.randint(0, len(sequence) - 1)
-                if 0 < j < len(sequence)-1:
-                    if sequence[j] != refRN and sequence[j+1] != refRN and sequence[j-1] != refRN:
+                if 0 < j < len(sequence) - 1:
+                    if sequence[j] != refRN and sequence[j + 1] != refRN and sequence[j - 1] != refRN:
                         sequence[i], sequence[j] = sequence[j], sequence[i]
                 elif j == 0:
-                    if sequence[j] != refRN and sequence[j+1] != refRN:
+                    if sequence[j] != refRN and sequence[j + 1] != refRN:
                         sequence[i], sequence[j] = sequence[j], sequence[i]
                 else:
-                    if sequence[j] != refRN and sequence[j-1] != refRN:
+                    if sequence[j] != refRN and sequence[j - 1] != refRN:
                         sequence[i], sequence[j] = sequence[j], sequence[i]
 
     return sequence
@@ -94,6 +99,7 @@ sequence_name_to_sounds = {
 
 }
 
+
 class Sequence:
     name: str
     sounds: list[str]
@@ -102,8 +108,8 @@ class Sequence:
     def __init__(self, name: str, isi: float):
         self.name = name
         self.isi = isi
-        if name == "RFRAM":
-            self.sounds = rfram_sequence()
+        if name == "RFRAM_1":
+            self.sounds = rfram_sequence_exp1()
         else:
             self.sounds = sequence_name_to_sounds[name]
 
@@ -118,11 +124,11 @@ class Sound:
         self.name = name
         self.sound = sound
         self.samplerate = samplerate
-        self.duration = duration #ms
+        self.duration = duration  # ms
 
         if sound is None:
             if name == "gaussian_N":
-                n_samples = samplerate * duration
+                n_samples = int(samplerate * duration/1000)
                 noise = np.random.normal(0, 1, n_samples)
 
                 # Applying hamming window: necessary??
@@ -135,8 +141,8 @@ class Sound:
 
                 self.sound = noise
 
-            elif name[:10] == "gaussian_R":
-                n_samples = int(samplerate * duration / 2)
+            elif name == "gaussian_RN" or name == "gaussian_RefRN":
+                n_samples = int(samplerate * duration / 2000)
                 noise = np.random.normal(0, 1, n_samples)
                 r_noise = np.concatenate((noise, noise))
 
@@ -150,8 +156,10 @@ class Sound:
 
                 self.sound = r_noise
 
-            elif name[:3] == "bip":
-                tone = np.sum(np.stack([librosa.tone(f, sr=samplerate, duration=duration / 1000) for f in fs], axis=0),axis=0)
+            elif name == "bip_1" or name == "bip_2":
+                # creating the tone with random distribution of frequency
+                dis = np.random.uniform(0, 1, fs.shape[0])
+                tone = np.transpose(np.transpose(np.stack([librosa.tone(f, sr=samplerate, duration=duration / 1000) for f in fs], axis=0)) @ np.transpose(dis))
                 # tone = librosa.tone(f, sr=samplerate, duration=duration_tone/1000)
 
                 # creating ramps
@@ -167,39 +175,80 @@ class Sound:
 
                 self.sound = tone
 
+            elif name == "silence":
+                silence = np.zeros(int(samplerate * duration/1000))
+
+                self.sound = silence
+
             else:
+                print("name: ", name)
                 raise ValueError("'name' is not defined")
-
-
 
 
 class Sound_pool:
     name: str
     sounds: dict[str, Sound]
+    fs: np.ndarray
 
     def __init__(self, name, fs=np.logspace(np.log(222), np.log(2000), 20, base=np.exp(1)), sounds=None):
         self.name = name
         self.sounds = sounds
+        self.fs = fs
 
         if sounds is None:
             dict_sounds = {}
             for key in list_sounds_name:
-                dict_sounds[key] = Sound(key, samplerates_sound[key], durations_sound[key], consines_rmp_length[key], fs)
+                # gaussian_N and gaussian_RN have to be different each time
+                if key == "gaussian_N" or key == "gaussian_RN":
+                    dict_sounds[key] = RFRAM_key
+                else:
+                    dict_sounds[key] = Sound(key, samplerates_sound[key], durations_sound[key],
+                                             consines_rmp_length[key], fs)
 
             self.sounds = dict_sounds
 
+    def __add__(self, other):
+        if isinstance(other, Sound_pool):
+            return Sound_pool(self.name + other.name, self.fs, {**self.sounds, **other.sounds})
+        else:
+            raise TypeError("unsupported operand type(s) for +: '{}' and '{}'".format(self.__class__, other.__class__))
 
-
+    def add(self, sound : Sound):
+        self.sounds[sound.name] = sound
 
 class Combinator:
     name: str
+    dirWav: str
+    samplerate: int
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, dirWav: str, samplerate: int):
         self.name = name
+        self.dirWav = dirWav
+        self.samplerate = samplerate
 
     def combine(self, seq_list: list[Sequence], sound_pool: Sound_pool):
+        """Combine the sounds in the sequence and generate a HuggingFace sound dataset."""
         for seq in seq_list:
-            pass
+            sound_seq = []
+            for sound in seq.sounds:
+                # gaussian_N and gaussian_RN have to be different each time
+                if sound_pool.sounds[sound] == RFRAM_key:
+                    newSound = Sound(sound, samplerates_sound[sound], durations_sound[sound],
+                                     consines_rmp_length[sound], sound_pool.fs)
+                    sound_seq.append(newSound.sound)
+                else:
+                    sound_seq.append(sound_pool.sounds[sound].sound)
+                sound_seq.append(np.zeros(int(self.samplerate * seq.isi)))
+                # concatenate the sounds
+            sound_seq = np.concatenate(sound_seq)
+
+            os.makedirs(os.path.join(self.dirWav, seq.name), exist_ok=True)
+            sf.write(os.path.join(self.dirWav, seq.name, seq.name + ".wav"), sound_seq, samplerate=self.samplerate)
+
+        # Generate the HuggingFace dataset with
+        dataset = load_dataset("/tests", data_files=os.path.join(self.dirWav, "*/*.wav"))
+        return dataset
+
 
 class Protocol:
     name: str
