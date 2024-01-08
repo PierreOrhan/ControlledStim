dec65190cdd4d9b3e42cdd90a6bdba6073ba674c
PierreOrhan <orhan.pierre.france@gmail.com>
2024-01-08 17:48:09 +0100

    simplified RandRegRand

--- sounds/perExperiment/protocols/AlRoumi2023.py
+++ sounds/perExperiment/protocols/AlRoumi2023.py
@@ -20,6 +20,9 @@ class RandRegRand_LOT(Protocol_independentTrial):
     lot_seq : str = "pairs"
     tones_fs : Union[list[float],np.ndarray] = field(default_factory=list)
 
+    s_rand: list[Sound] = field(default=None)
+    s_reg: list[Sound] = field(default=None)
+
     def __post_init__(self):
         self.name = self.name+"_"+self.lot_seq
         sounds = [Bip(name="bip-" + str(idf), samplerate=self.samplerate, duration=self.duration_tone, fs=[f]) for
@@ -35,10 +38,12 @@ class RandRegRand_LOT(Protocol_independentTrial):
 
     def _getPoolAndSeq(self) -> Tuple[list[Sound_pool],list[Sequence]]:
         ## Instantiate the vocabularies:
-        s_rand = Sound_pool.from_list(self.sound_pool.pick_norepeat_n(16))
-        s_reg = Sound_pool.from_list(s_rand.pick_norepeat_n(2))
-
-        all_pool = [s_rand] + [s_reg for _ in range(self.motif_repeat)] + [s_reg]
+        if self.s_rand is None:
+            all_pool = [self.s_rand] + [self.s_reg for _ in range(self.motif_repeat)] + [self.s_reg]
+        else:
+            s_rand = Sound_pool.from_list(self.sound_pool.pick_norepeat_n(16))
+            s_reg = Sound_pool.from_list(s_rand.pick_norepeat_n(2))
+            all_pool = [s_rand] + [s_reg for _ in range(self.motif_repeat)] + [s_reg]
         all_seq = [self.randSeq] + [self.regSeq for _ in range(self.motif_repeat)] + [self.randSeqEnd]
         return all_pool,all_seq
     def _trial(self) -> tuple[list[Sound],int,pd.DataFrame]:
@@ -62,6 +67,23 @@ class RandRegRand_LOT(Protocol_independentTrial):
 
         return (all_sound,nb_element,df_info)
 
+    def samplePool(self) -> Tuple[list[Sound], list[Sound]]:
+        assert self.s_rand is None
+        self.s_rand = Sound_pool.from_list(self.sound_pool.pick_norepeat_n(16))
+        self.s_reg = Sound_pool.from_list(self.s_rand.pick_norepeat_n(2))
+        return self.s_rand, self.s_reg
+
+    def fixPoolSampled(self, s_rand: list[Sound], s_reg: list[Sound]) -> None:
+        self.s_rand = s_rand
+        self.s_reg = s_reg
+    def sampleBoundPool(self,min_freqDist:float,max_freqDist:float) -> Tuple[list[Sound],list[Sound]]:
+        assert self.s_rand is None
+        self.s_rand  = Sound_pool.from_list(self.sound_pool.pick_norepeat_n(16))
+        self.sound_pool.clear_picked()
+        self.s_reg = Sound_pool.from_list(self.sound_pool.boundpick_norepeat_n(2,min_freqDist,max_freqDist,"first_freq"))
+        return self.s_rand,self.s_reg
+
+
 @dataclass
 class RandRegRand_LOT_deviant(RandRegRand_LOT):
     deviant : int = 0
@@ -76,11 +98,7 @@ class RandRegRand_LOT_deviant(RandRegRand_LOT):
         self.devSeq.as_deviant_pattern(self.deviant)
 
     def _getPoolAndSeq(self) -> Tuple[list[Sound_pool], list[Sequence]]:
-        ## Instantiate the vocabularies:
-        s_rand = Sound_pool.from_list(self.sound_pool.pick_norepeat_n(16))
-        s_reg = Sound_pool.from_list(s_rand.pick_norepeat_n(2))
-
-        all_pool = [s_rand] + [s_reg for _ in range(self.motif_repeat)] + [s_reg]
+        all_pool, _ = super(RandRegRand_LOT_deviant)._getPoolAndSeq()
         all_seq = [self.randSeq] + [self.regSeq for _ in range(self.motif_repeat)] + [self.devSeq]
         return all_pool, all_seq
 
@@ -89,42 +107,20 @@ class RandRegRand_LOT_deviant(RandRegRand_LOT):
         df_info = df_info.join(pd.DataFrame({"deviantpos":[self.devSeq.deviant_pos[self.deviant]],"deviant":[self.deviant]}).set_axis(df_info.index))
         return (all_sound,nb_element,df_info)
 
-@dataclass
-class RandRegRand_LOT_deviant_fixedPool(RandRegRand_LOT_deviant):
-    s_rand : list[Sound] = field(default=None)
-    s_reg : list[Sound] = field(default=None)
-    def samplePool(self) -> Tuple[list[Sound],list[Sound]]:
-        assert self.s_rand is None
-        self.s_rand  = Sound_pool.from_list(self.sound_pool.pick_norepeat_n(16))
-        self.s_reg = Sound_pool.from_list(self.s_rand.pick_norepeat_n(2))
-        return self.s_rand,self.s_reg
-    def fixPoolSampled(self,s_rand: list[Sound],s_reg: list[Sound]) -> None:
-        self.s_rand = s_rand
-        self.s_reg = s_reg
-    def _getPoolAndSeq(self) -> Tuple[list[Sound_pool], list[Sequence]]:
-        ## Instantiate the vocabularies:
-        all_pool = [self.s_rand] + [self.s_reg for _ in range(self.motif_repeat)] + [self.s_reg]
-        all_seq = [self.randSeq] + [self.regSeq for _ in range(self.motif_repeat)] + [self.devSeq]
-        return all_pool, all_seq
-
 
 @dataclass
-class RandRegRand_LOT_deviant_BoundFixedPool(RandRegRand_LOT_deviant_fixedPool):
+class RandReg_LOT(RandRegRand_LOT):
     def __post_init__(self):
         sounds = [Bip(name="bip-"+str(idf),samplerate=self.samplerate, duration=self.duration_tone, fs=[f]) for idf,f in enumerate(self.tones_fs)]
         # Note: naming the bip is useful to one who is where.
         self.sound_pool = Sound_pool.from_list(sounds)
         self.randSeq = ToneList(isi=self.isi, cycle=16)
         self.regSeq = lot_patterns[self.lot_seq](isi=self.isi)
+    def _getPoolAndSeq(self) -> Tuple[list[Sound_pool], list[Sequence]]:
+        all_pool, _ = super(RandRegRand_LOT_deviant)._getPoolAndSeq()
+        all_seq = [self.randSeq] + [self.regSeq for _ in range(self.motif_repeat)] + [self.regSeq]
+        return all_pool, all_seq
 
-        self.devSeq = lot_patterns[self.lot_seq](isi=self.isi)
-        self.devSeq.as_deviant_pattern(self.deviant)
-    def samplePool(self,min_freqDist:float,max_freqDist:float) -> Tuple[list[Sound],list[Sound]]:
-        assert self.s_rand is None
-        self.s_rand  = Sound_pool.from_list(self.sound_pool.pick_norepeat_n(16))
-        self.sound_pool.clear_picked()
-        self.s_reg = Sound_pool.from_list(self.sound_pool.boundpick_norepeat_n(2,min_freqDist,max_freqDist,"first_freq"))
-        return self.s_rand,self.s_reg
 
 @dataclass
 class RandRegRand_LOT_Generalize(RandRegRand_LOT):
